976db7e4bc02d23c7aae166127091096751ab5af
Petar Velkov <petar.velkov@securax.org>
2017-01-16 00:06:47 +0200

    Replaced TMP36 with DHT22

--- heater/heater.ino
+++ heater/heater.ino
@@ -1,18 +1,26 @@
+#include <DHT.h>
+#include <DHT_U.h>
+
 #include <EEPROMex.h>
 #include <EEPROMVar.h>
 
-int pinTemp = 0;
-int pinLow = 7;
-int pinMid = 8;
-int pinHigh = 9;
+#define PINDHT 2
+#define PINLOW 7
+#define PINMID 8
+#define PINHIGH 9
+#define DHTTYPE DHT22
+#define INTERVAL 10000    // Interval between measurements
 
+float humCurr;
 float tempCurrC;
-float tempTargetC = 16.8;
+float tempTargetC = 21.1;
 
 int p_restore = 0;
 int p_tempTargetC = 4;
 
 int currSpeed;
+
+DHT dht(PINDHT, DHTTYPE);
 
 void fanSet(int speed) {
   Serial.print("Current temperature is ");
@@ -27,24 +35,24 @@ void fanSet(int speed) {
 
   switch (speed) {
     case 0:
-      digitalWrite(pinLow, LOW);
-      digitalWrite(pinMid, LOW);
-      digitalWrite(pinHigh, LOW);
+      digitalWrite(PINLOW, LOW);
+      digitalWrite(PINMID, LOW);
+      digitalWrite(PINHIGH, LOW);
       break;
     case 1:
-      digitalWrite(pinLow, HIGH);
-      digitalWrite(pinMid, LOW);
-      digitalWrite(pinHigh, LOW);
+      digitalWrite(PINLOW, HIGH);
+      digitalWrite(PINMID, LOW);
+      digitalWrite(PINHIGH, LOW);
       break;
     case 2:
-      digitalWrite(pinLow, LOW);
-      digitalWrite(pinMid, HIGH);
-      digitalWrite(pinHigh, LOW);
+      digitalWrite(PINLOW, LOW);
+      digitalWrite(PINMID, HIGH);
+      digitalWrite(PINHIGH, LOW);
       break;
     case 3:
-      digitalWrite(pinLow, LOW);
-      digitalWrite(pinMid, LOW);
-      digitalWrite(pinHigh, HIGH);
+      digitalWrite(PINLOW, LOW);
+      digitalWrite(PINMID, LOW);
+      digitalWrite(PINHIGH, HIGH);
       break;
     default:
       Serial.println("Unknown fan speed given. Working options are 1 (low), 2 (mid) and 3 (high).");
@@ -57,11 +65,11 @@ void setup() {
   EEPROM.write(p_restore, false);
 
   Serial.begin(9600);
-  analogReference(EXTERNAL);
+  dht.begin();
 
-  pinMode(pinLow, OUTPUT);
-  pinMode(pinMid, OUTPUT);
-  pinMode(pinHigh, OUTPUT);
+  pinMode(PINLOW, OUTPUT);
+  pinMode(PINMID, OUTPUT);
+  pinMode(PINHIGH, OUTPUT);
 
   bool restore = EEPROM.read(p_restore);
 
@@ -80,36 +88,43 @@ void setup() {
 }
 
 void loop() {
-  int readTemp = analogRead(pinTemp);
-  float voltage = readTemp * 3.3 / 1024.0;
+  delay(INTERVAL);
+  
   float dT;
 
-  tempCurrC = (voltage - 0.5) * 100;
+  humCurr = dht.readHumidity();
+  tempCurrC = dht.readTemperature();
   dT = tempTargetC - tempCurrC;
+
+//  if (isnan(tempCurrC)) {
+//    Serial.println("Failed to read from DHT sensor");
+//    return;
+//  }
 
   Serial.print(tempCurrC);
   Serial.print(" degC, target is ");
   Serial.print(tempTargetC);
   Serial.print(" degC, dT = ");
-  Serial.println(dT);
+  Serial.print(dT);
+  Serial.print(", humidity is ");
+  Serial.print(humCurr);
+  Serial.println("%");
 
   if (dT > 3) {
     if (currSpeed != 3) {
       fanSet(3);
     }
-  } else if (dT > 2) {
+  } else if (dT > 1.6) {
     if (currSpeed != 2) {
       fanSet(2);
     }
-  } else if (dT > 1) {
+  } else if (dT > 0.3) {
     if (currSpeed != 1) {
       fanSet(1);
     }
-  } else if (dT < -1) {
+  } else if (dT < -0.3) {
     if (currSpeed != 0) {
       fanSet(0);
     }
   }
-
-  delay(10000);
 }
